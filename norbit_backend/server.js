1d200fb12ea1bba13fa13a46e8bd9f0b90f20eb1
A-Bolf <hosc19@gmail.com>
2022-11-16 16:53:37 +0100

    add websocket streaming to backend + data display on frontend

--- norbit_backend/server.js
+++ norbit_backend/server.js
@@ -1,16 +1,82 @@
-const http = require("http");
-
-const hostname = "127.0.0.1";
-const port = 5000;
-
+const Fs = require("fs");
+const CsvReadableStream = require("csv-reader");
+const AutoDetectDecoderStream = require("autodetect-decoder-stream");
 const WebSocket = require("ws");
 const wss = new WebSocket.Server({ port: 8080 });
 const clients = new Map();
+let sendinterval;
+let boatStream1 = Fs.createReadStream("norbit_backend/lines/line1.csv").pipe(
+  new AutoDetectDecoderStream({ defaultEncoding: "1255" })
+);
+let boatStream2 = Fs.createReadStream("norbit_backend/lines/line2.csv").pipe(
+  new AutoDetectDecoderStream({ defaultEncoding: "1255" })
+);
+let boatStream3 = Fs.createReadStream("norbit_backend/lines/line3.csv").pipe(
+  new AutoDetectDecoderStream({ defaultEncoding: "1255" })
+);
+let boat1 = [];
+let boat2 = [];
+let boat3 = [];
+const readData = (inputStream, arr) => {
+  inputStream
+    .pipe(
+      new CsvReadableStream({
+        skipHeader: true,
+        parseNumbers: true,
+        parseBooleans: true,
+        trim: true,
+      })
+    )
+    .on("data", function (row) {
+      arr.push({ lat: row[0], lon: row[1], heading: row[2] });
+    })
+    .on("end", function () {
+      console.log("read done");
+    });
+};
+const readAllData = (streams, arrs) => {
+  for (let i = 0; i < arrs.length; i++) {
+    readData(streams[i], arrs[i]);
+  }
+};
+readAllData([boatStream1, boatStream2, boatStream3], [boat1, boat2, boat3]);
+
+const sendData = (ws, arrs) => {
+  emptyArrCount = 0;
+  for (let i = 0; i < arrs.length; i++) {
+    if ((Array.length = 0)) {
+      emptyArrCount++;
+    }
+  }
+  if (emptyArrCount === arrs.length) {
+    clearInterval(sendinterval);
+    return;
+  }
+  ws.send(
+    JSON.stringify({
+      boat1: arrs[0].shift(),
+      boat2: arrs[1].shift(),
+      boat3: arrs[2].shift(),
+    })
+  );
+  console.log(
+    JSON.stringify({
+      boat1: arrs[0].shift(),
+      boat2: arrs[1].shift(),
+      boat3: arrs[2].shift(),
+    })
+  );
+};
+
 wss.on("connection", (ws) => {
   const id = uuidv4();
   const metadata = { id };
   clients.set(ws, metadata);
-  ws.send("WS CONNECTED");
+  if (sendinterval === undefined && clients.length !== 0) {
+    sendinterval = setInterval(() => {
+      sendData(ws, [boat1, boat2, boat3]);
+    }, 1000);
+  }
   ws.on("close", () => {
     clients.delete(ws);
   });
@@ -23,4 +89,5 @@ function uuidv4() {
     return v.toString(16);
   });
 }
+
 console.log("wss up");
--- norbit_frontend/src/App.js
+++ norbit_frontend/src/App.js
@@ -1,13 +1,40 @@
 import "./App.css";
 import { useState } from "react";
+const socket = new WebSocket("ws://127.0.0.1:8080");
 
 function App() {
-  const socket = new WebSocket("ws://127.0.0.1:8080");
   socket.onmessage = ({ data }) => {
+    data = JSON.parse(data);
     console.log(data);
+    if (data.boat1 !== undefined) {
+      setBoat1(data.boat1);
+    }
+    if (data.boat2 !== undefined) {
+      setBoat2(data.boat2);
+    }
+    if (data.boat3 !== undefined) {
+      setBoat3(data.boat3);
+    }
+    if (JSON.stringify(data) === "{}") {
+      socket.close();
+    }
   };
-  const [data, setData] = useState(["asd"]);
-  return <div className="App">{data}</div>;
+  const [boat1, setBoat1] = useState("NO DATA");
+  const [boat2, setBoat2] = useState("NO DATA");
+  const [boat3, setBoat3] = useState("NO DATA");
+  return (
+    <>
+      <div>
+        boat1 pos: lat: {boat1.lat} lon: {boat1.lon} heading:{boat1.heading}
+      </div>
+      <div>
+        boat2 pos: lat: {boat2.lat} lon: {boat2.lon} heading:{boat2.heading}
+      </div>
+      <div>
+        boat3 pos: lat: {boat3.lat} lon: {boat3.lon} heading:{boat3.heading}
+      </div>
+    </>
+  );
 }
 
 export default App;
